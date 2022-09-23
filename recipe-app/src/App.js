f96dc7c07bf9487e7514918ca869418bdaf4009c
iglesias-johnluke <iglesiasjohnluke@gmail.com>
2022-09-23 10:26:15 -0400

    search recipes

--- recipe-app/src/App.js
+++ recipe-app/src/App.js
@@ -1,23 +1,71 @@
 import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import Recipe from "./Recipe"
 
-function App() {
+const App = () => {
+  
+  const [counter, setCounter] = useState(0); //tracks how many times counter header is touched
+  const[recipes, setRecipes] = useState([]); //recipes is fetched list of recipe objects
+  const [search, setSearch] = useState("") //tracks search bar text
+  const [query, setQuery] = useState("chicken")//tracks what user is searching in search bar
+
+  const APP_ID = "11c3eefe";
+  const APP_KEY = "404cc8c9e05afe70e3cfea123499954e";
+
+  //update fetch url query parameter according to query state
+  const exampleRequest = `https://api.edamam.com/search?q=${query}&app_id=11c3eefe&app_key=404cc8c9e05afe70e3cfea123499954e`;
+
+
+  
+
+  useEffect( () => {
+    getRecipes()
+  }, []);
+  
+  
+//fetch data, initialize recipe variable
+  async function getRecipes(){
+    let response = await fetch(exampleRequest);
+    let data = await response.json();
+    console.log(data)
+    setRecipes(data.hits) 
+  }
+  
+  function updateSearch(newText){
+    setSearch(newText.target.value)
+  }
+
+  //called whenever we submit form text/query/search
+  //only update url fetch query when we press search (sets searchbar text to query value)
+  function getSearch(event){
+    event.preventDefault() //prevent page refresh
+    setQuery(search)
+  }
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className='search-form' onSubmit={getSearch}>
+        <input type="text" className='search-bar' value={search} onChange={updateSearch} />
+        <button 
+          type='submit' 
+          className='search-button'>{counter}
+          
+        </button>
+        <h1 onClick={() => setCounter(counter + 1) }>{counter}</h1>
+        {recipes.map(
+          recipe => 
+            <Recipe 
+            key={recipe.recipe.label} //key gets rid of unique property error
+            title={recipe.recipe.label}
+            calories={recipe.recipe.calories}
+            image={recipe.recipe.image}
+            />
+            
+          ) 
+        }
+      </form>
     </div>
   );
 }
